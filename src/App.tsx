9a5eb3bbbd90b6f670459a66f3b8345eef025e39
myhzy <notmyhzy@hotmail.com>
2025-10-09 18:49:46 +0100

    Initial commit: setup React + Tailwind weather app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-sky-50 to-sky-200">
+      <h1 className="text-1xl font-extrabold text-sky-600 drop-shadow-md">
+        Tailwind is Working! 🎉
+      </h1>
+    </div>
+  );
+}
+
+export default App;
