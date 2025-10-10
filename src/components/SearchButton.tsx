0dcc21eb79e59a3d0eac3af2024579fe47274368
myhzy <notmyhzy@hotmail.com>
2025-10-10 16:10:26 +0100

    added search box implementation routing to api

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import { useWeather } from "../hooks/useWeather";
 
 export default function SearchButton() {
   const { weather, fetchWeather } = useWeather();
+  const [city, setCity] = useState("");
 
-  console.log(weather);
+  function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      fetchWeather(city.trim());
+      setCity("");
+    }
+  }
 
   return (
     <div className="rounded-[10px] w-[200px] h-[30px] flex items-center justify-center bg-white/10 border border-white/20 mb-[10px]">
@@ -11,6 +18,9 @@ export default function SearchButton() {
         type="text"
         placeholder="Search your city..."
         className="bg-transparent text-white text-center w-full h-full outline-none placeholder-white"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyDown={handleKeyPress}
       />
     </div>
   );
